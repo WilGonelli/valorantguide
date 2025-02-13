446c03b14f2e95a57969ea730e0fd9a840e1614f
wilgonelli <wil_gonelli@hotmail.com>
2025-02-13 20:57:25 -0300

    deploy para o github pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
-  // images: {
-  //   unoptimized: true,
-  // },
-  // basePath: "/valorantguide",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/valorantguide",
 };
 
 module.exports = nextConfig;
